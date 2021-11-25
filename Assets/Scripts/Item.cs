31b6e74c6be848950ac8d25c33aa8669be3da7a7
Jess Hammer <59108399+jess-hammer@users.noreply.github.com>
2021-11-25 17:01:19 +1000

    Removed size attribute

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -14,7 +14,6 @@ public class Item
     public float usefulnessPercent; // Parameter to quantify the usefulness of the item
     public float happinessPercent; // Parameter to quantify the beauty, deliciousness, or usefulness or other nice qualites
     public float weightValue; // Parameter in kg
-    public float sizeValue; // Parameter in cm
     public float productionChance;
 
     public int consumeRate; // days between consumption
@@ -24,13 +23,12 @@ public class Item
     static float happinessWeighting = 0.4f; // Parameter
     static int valueScale = 10; // Parameter to scale the value of items up into more appropriate range
     
-    public Item(ItemName itemName, float necessity, float usefulness, float happiness, float weight, float size, int consumeRate, float productionChance) {
+    public Item(ItemName itemName, float necessity, float usefulness, float happiness, float weight, int consumeRate, float productionChance) {
         this.itemName = itemName;
         necessityPercent = necessity;
         usefulnessPercent = usefulness;
         happinessPercent = happiness;
         weightValue = weight;
-        sizeValue = size;
         this.consumeRate = consumeRate;
         this.productionChance = productionChance;
 
@@ -68,15 +66,15 @@ public class Item
 
 public class ItemStash {
     public Item [] items = new Item[] {
-        new Item(ItemName.VEGETABLE, 1, 0, 0.2f, 0.5f, 20, (int)Random.Range(2f, 4f), Random.Range(0.5f, 1f)),
-        new Item(ItemName.GRAIN, 1, 0, 0.2f, 2, 20, (int)Random.Range(2f, 4f), Random.Range(0.5f, 1f)),
-        new Item(ItemName.MEAT, 1, 0, 0.7f, 2, 20, (int)Random.Range(3f, 6f), Random.Range(0.5f, 1f)),
-        new Item(ItemName.TRINKET, 0, 0, 0.8f, 0.1f, 5, (int)Random.Range(5f, 11f), Random.Range(0, 0.5f)),
-        new Item(ItemName.PEBBLE, 0, 0, 0, 0.1f, 2, (int)Random.Range(100f, 200f), 1),
-        new Item(ItemName.TOOL, 0, 1, 0.1f, 10, 20, (int)Random.Range(5f, 11f), Random.Range(0, 0.5f)),
-        new Item(ItemName.FURNITURE, 0.2f, 0.4f, 0.2f, 30, 100, (int)Random.Range(7f, 15f), Random.Range(0, 0.5f)),
-        new Item(ItemName.CLOTHING, 0.2f, 0.4f, 0.2f, 1, 50, (int)Random.Range(5f, 11f), Random.Range(0, 0.5f)),
-        new Item(ItemName.TOILET_PAPER, 0.2f, 0.1f, 0f, 0.1f, 10, (int)Random.Range(4f, 8f), Random.Range(0, 0.5f))
+        new Item(ItemName.VEGETABLE, 1, 0, 0.2f, 0.5f, (int)Random.Range(2f, 4f), Random.Range(0.5f, 1f)),
+        new Item(ItemName.GRAIN, 1, 0, 0.2f, 2, (int)Random.Range(2f, 4f), Random.Range(0.5f, 1f)),
+        new Item(ItemName.MEAT, 1, 0, 0.7f, 2, (int)Random.Range(3f, 6f), Random.Range(0.5f, 1f)),
+        new Item(ItemName.TRINKET, 0, 0, 0.8f, 0.5f, (int)Random.Range(7f, 15f), Random.Range(0, 0.5f)),
+        new Item(ItemName.PEBBLE, 0, 0, 0, 0.1f, (int)Random.Range(1000f, 2000f), 1),
+        new Item(ItemName.TOOL, 0, 1, 0.1f, 10, (int)Random.Range(5f, 11f), Random.Range(0, 0.5f)),
+        new Item(ItemName.FURNITURE, 0.2f, 0.4f, 0.2f, 30, (int)Random.Range(7f, 15f), Random.Range(0, 0.5f)),
+        new Item(ItemName.CLOTHING, 0.2f, 0.4f, 0.2f, 1, (int)Random.Range(5f, 11f), Random.Range(0, 0.5f)),
+        new Item(ItemName.TOILET_PAPER, 0.2f, 0.1f, 0f, 0.1f, (int)Random.Range(4f, 8f), Random.Range(0, 0.5f))
     };
 }
 
